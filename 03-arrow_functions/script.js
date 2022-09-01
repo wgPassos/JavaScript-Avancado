5871538258941e54e64fbfc89ae04cc89ca80fc0
Willian Passos <passoswilliangabriel@gmail.com>
2022-08-31 23:30:02 -0300

    Aroow functions e aprendendo callback

--- 03-arrow_functions/script.js
+++ 03-arrow_functions/script.js
@@ -1,2 +1,15 @@
 // Vantagens da Arraow Functios -> O código fica menor e mais simples
 // Desvantagens -> Não entende o .bind()  e perde o contexto para o this - enxergar apenas o contexto global 
+
+let dobro = x => 2 * x;
+
+// let dobro = function () {
+//     return 2 * this.x
+// }
+
+let numero = {
+    x: 8,
+    d: dobro
+}
+
+console.log(numero.d());
