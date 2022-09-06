a87adfd7aeeedaa357b5c5beca531ba567694a05
Willian Passos <passoswilliangabriel@gmail.com>
2022-09-06 06:41:03 -0300

    Mais sobre valor e referência por atribuição (Array e Object)

--- 09-map_valor_referecencia/script.js
+++ 09-map_valor_referecencia/script.js
@@ -1,7 +1,21 @@
 let alunoA = { nome: "Joao", idade: 14};
 
 let alunoB = alunoA;
-
 alunoB.idade = 18;
 
 console.log(alunoB, alunoA);
+
+// No JavaScript o "objeto" é passado como referência
+// O que quer dizer isso?
+
+
+
+let turmaA = ["pedor", "Pedro", "Marcos"];
+
+let turmaB = turmaA;
+turmaB= "Jose";
+
+console.log(turmaA)
+console.log(turmaB)
+console.log(turmaA)
+
