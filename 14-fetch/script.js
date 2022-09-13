88afcf5d80eb7eb2fc308788226f948ec5d665b8
Willian Passos <passoswilliangabriel@gmail.com>
2022-09-12 23:44:56 -0300

    Aprimorando fetch api - conversor de moedas

--- 14-fetch/script.js
+++ 14-fetch/script.js
@@ -2,9 +2,10 @@ let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL"
 
 fetch(url)
     .then((res) => {
-        // console.log(res);
+        console.log(res);
         return res.json();
     })
     .then((data) => {
         console.log(data);
+        console.log(data.USDBRL.bid)
     })
