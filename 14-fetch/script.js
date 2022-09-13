c131e9ec8ee63c123f38246b1bf11ce7471015e0
Willian Passos <passoswilliangabriel@gmail.com>
2022-09-13 00:31:21 -0300

    Retornando valor do Dolar em Real

--- 14-fetch/script.js
+++ 14-fetch/script.js
@@ -1,11 +1,25 @@
-let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL"
+let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 
-fetch(url)
+
+
+
+function captaNumber () {
+    let input = document.getElementById("numberCap");
+    let numberValue = input.value;
+    console.log(numberValue)
+
+    fetch(url)
     .then((res) => {
         console.log(res);
         return res.json();
     })
     .then((data) => {
         console.log(data);
-        console.log(data.USDBRL.bid)
+        console.log(data.USDBRL.bid);
+        let bid = data.USDBRL.bid;
+
+        let resultado = `${numberValue} dolares = ${numberValue * bid} em reais`;
+        document.querySelector("#result").innerHTML = resultado;
+
     })
+}
