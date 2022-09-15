e71e73a711a3059f8c941187afee3f44785df34a
Willian Passos <passoswilliangabriel@gmail.com>
2022-09-14 23:56:32 -0300

    Acertando api com fetch

--- 14-fetch/script.js
+++ 14-fetch/script.js
@@ -5,20 +5,21 @@ let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 
 function captaNumber () {
     let input = document.getElementById("numberCap");
-    let numberValue = input.value;
-    console.log(numberValue)
+    let valor = input.value;
+    console.log(valor);
 
     fetch(url)
     .then((res) => {
-        console.log(res);
-        return res.json();
+        // console.log(res);
+        return res.json()
     })
     .then((data) => {
         console.log(data);
         console.log(data.USDBRL.bid);
         let bid = data.USDBRL.bid;
 
-        let resultado = `${numberValue} dolares = ${numberValue * bid} em reais`;
+        let resultado = `${valor} dolares = ${(valor * bid).toFixed(2)} em reais`;
+        // let resultadoToFixed = parseFloat(resultado).toFixed(2);
         document.querySelector("#result").innerHTML = resultado;
 
     })
